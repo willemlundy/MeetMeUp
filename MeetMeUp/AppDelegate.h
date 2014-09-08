9999d5737ff4e85627b02debc156271831fcb96f
Dave Krawczyk <dave.krawczyk@gmail.com>
2014-09-08 09:23:00 -0500

    Initial Commit

---
+++ MeetMeUp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MeetMeUp
+//
+//  Created by Dave Krawczyk on 9/8/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MeetMeUp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MeetMeUp
+//
+//  Created by Dave Krawczyk on 9/8/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
